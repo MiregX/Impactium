b457d4f9b67e8c0ab1604e6c0a1843241c755df5
Mireg <markgerasimchuk8@gmail.com>
2023-10-08 15:55:03 +0200

    Исправлены ошибки EJS
    В дизайне и оверфлове стилей

---
+++ modules/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const utils = require('../utils');
+
+router.get('/', (request, response) => {
+});
+
+module.exports = router;
