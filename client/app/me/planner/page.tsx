7cb5fc607b99c8aa3ab722bf66801689268b31fb
Takemyeye <mellovan2005@gmail.com>
2024-03-16 15:46:01 +0100

    Break<Planner-text-dinamic-test>

--- client/app/me/planner/page.tsx
+++ client/app/me/planner/page.tsx
@@ -1,26 +1,63 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { Task } from '@/components/me/planner/Task'
+import { Task } from '@/components/me/planner/Task';
 import styles from '@/styles/me/Planner.module.css';
 
 export default function PlannerPage() {
   const tasksFromLocalStorage = localStorage.getItem('tasks');
+  const [displayText, setDisplayText] = useState('');
+  const [textIndex, setTextIndex] = useState(0);
+
+  const textsToIterate = [
+    'Привет!',
+    'Я Олег.',
+    'Хочу сказать, что ТимЛид ЛОХ!!!'
+  ];
+  const interval = 100;
+  let changeInterval = 1000; 
+
+  useEffect(() => {
+    let index = 0;
+    const textLength = textsToIterate[textIndex].length;
+
+    const displayTextInterval = setInterval(() => {
+      if (index < textLength) {
+        setDisplayText((prevText) => prevText + textsToIterate[textIndex].charAt(index));
+        index++;
+      } else {
+        clearInterval(displayTextInterval);
+        // Здесь устанавливаем новый интервал после появления динамического текста
+        const changeTextTimeout = setTimeout(() => {
+          setTextIndex((prevIndex) => (prevIndex + 1) % textsToIterate.length);
+          setDisplayText(''); 
+          changeInterval = 1000; 
+        }, changeInterval);
+      }
+    }, interval);
+
+    return () => clearInterval(displayTextInterval);
+  }, [textIndex]);
+
+  useEffect(() => {
+    setDisplayText(textsToIterate[textIndex].charAt(0));
+  }, [textIndex]);
+
   if (typeof tasksFromLocalStorage === 'number') {
     localStorage.removeItem('tasks');
   }
+
   const [tasks, setTasks] = useState(tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : []);
   const [active, setActive] = useState(null);
-  console.log(tasks);
 
-  useEffect(() => localStorage.setItem('tasks', JSON.stringify(tasks)), [tasks])
+  useEffect(() => localStorage.setItem('tasks', JSON.stringify(tasks)), [tasks]);
 
   const addTask = () => {
     const newTask = {
       id: tasks.length + 1,
       status: 200,
-      title: 'SOMETHING'
+      title: 'SOMETHING',
     };
-  
+
     setTasks((oldTasks) => [...oldTasks, newTask]);
   };
 
@@ -30,22 +67,29 @@ export default function PlannerPage() {
       return updatedTasks;
     });
   }
-  
+
   return (
     <div className={styles.planner}>
       {tasks.map((task, index) => (
         <Task
+          key={task.id}
           tasks={tasks}
           active={active}
           setActive={setActive}
           task={task}
           setTasks={setTasks}
           index={index}
-          />
+        />
       ))}
 
-      <button className={styles.addtasks} onClick={addTask}>Добавить задачу</button>
-      <button className={styles.removetask} onClick={removeLastTask}>Убрать последнюю задачу</button>
+      <button className={styles.addtasks} onClick={addTask}>
+        Добавить задачу
+      </button>
+      <button className={styles.removetask} onClick={removeLastTask}>
+        Убрать последнюю задачу
+      </button>
+
+      <div className={styles.dynamicText}>{displayText}</div>
     </div>
   );
 }
