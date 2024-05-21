83495f669c1b7939b7d045fedc3ae54812b70d08
Mireg <markgerasimchuk8@gmail.com>
2024-05-22 00:57:54 +0200

    /

---
+++ client/app/teams/components/SearchBar.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { Input } from '@/ui/Input'
+import s from '../Teams.module.css'
+import { useLanguage } from '@/context/Language'
+import { _server } from '@/dto/master';
+import { useState } from 'react';
+import { Team } from '@/dto/Team';
+import { GeistButton, GeistButtonTypes } from '@/ui/GeistButton';
+import { useMessage } from '@/context/Message';
+import CreateTeam from '@/banners/create_team/CreateTeam';
+
+export function SearchBar({ search, setSearch, setTeams, teams }) {
+  const { lang } = useLanguage();
+  const [loading, setLoading] = useState(false);
+  const { spawnBanner } = useMessage();
+  let searchTimeout: number | any;
+  
+  const findTeams = async (value: string): Promise<Team[]> => {
+    setLoading(true)
+    const response = await fetch(`${_server()}/api/team/find/${value}`, {
+      method: 'GET',
+      next: {
+        revalidate: 60
+      }
+    });
+    setLoading(false)
+    return response.ok ? await response.json() : [];
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+    if (event.target.value.length >= 3) {
+      clearTimeout(searchTimeout);
+      searchTimeout = setTimeout(() => {
+        findTeams(event.target.value.toLowerCase()).then((teams) => {
+          setTeams(mergeTeams(teams));
+        });
+      }, 1000);
+    }
+  };
+
+  const mergeTeams = (newTeams: Team[] ): Team[] => {
+    const filteredTeams = teams.filter((team: Team) => !newTeams.some(newTeam => newTeam.indent === team.indent));
+
+    return [...newTeams, ...filteredTeams];
+  };  
+
+  return (
+    <div className={s.bar}>
+    <Input
+      image={loading
+        ? 'https://cdn.impactium.fun/ui/action/redo.svg'
+        : 'https://cdn.impactium.fun/ui/specific/mention.svg'
+      }
+      placeholder={lang.team.enter_indent_or_title}
+      aria-label="Search"
+      aria-invalid="false"
+      type="search"
+      value={search}
+      style={[loading && s.loading]}
+      onChange={handleSearchChange}
+    />
+    <GeistButton
+      options={{
+        type: GeistButtonTypes.Button,
+        do: () => spawnBanner(<CreateTeam />),
+        text: lang.create.team,
+        focused: true,
+        style: [s.button]
+      }}
+    />
+  </div>
+  )
+}
--- client/app/teams/page.tsx
+++ client/app/teams/page.tsx
@@ -1,102 +1,41 @@
 'use client'
-import { useEffect, useState } from 'react';
-import { Input } from '@/ui/Input';
+import { useState } from 'react';
 import { PanelTemplate } from '@/components/main/PanelTempate';
 import { _server } from '@/dto/master';
 import { useLanguage } from '@/context/Language';
 import { useMessage } from '@/context/Message';
-import { GeistButton, GeistButtonTypes } from '@/ui/GeistButton';
-import CreateTeam from '@/banners/create_team/CreateTeam';
 import { TeamUnit } from './components/TeamUnit';
 import s from './Teams.module.css';
 import { Panel } from '@/ui/Panel';
 import { Team } from '@/dto/Team';
 import { useUser } from '@/context/User';
+import { SearchBar } from './components/SearchBar';
 
 export default function TeamsPage({ data }) {
   const { lang } = useLanguage();
-  const { spawnBanner } = useMessage();
   const [teams, setTeams] = useState(data);
   const { user } = useUser();
-  const [loading, setLoading] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
-  let searchTimeout: number | any;
-
-  const fetchTeams = async (type: 'find' | 'get', value?: string): Promise<Team[]> => {
-    setLoading(true)
-    const response = await fetch(`${_server()}/api/team/${type}/${value}`, {
-      method: 'GET',
-      next: {
-        revalidate: 60
-      }
-    });
-    setLoading(false)
-    return response.ok ? await response.json() : [];
-  };
-
-  const userTeams = fetch('')
-
-  useEffect(() => {
-    if (!data) {
-      fetchTeams('get').then((teams) => {
-        setTeams(mergeTeams(teams));
-      });
-    }
-  }, [data]);
-
-  const handleSearchChange = (event) => {
-    setSearchValue(event.target.value);
-    if (event.target.value.length >= 3) {
-      clearTimeout(searchTimeout);
-      searchTimeout = setTimeout(() => {
-        fetchTeams('find', event.target.value.toLowerCase()).then((teams) => {
-          setTeams(mergeTeams(teams));
-        });
-      }, 1000);
-    }
-  };
-
-  const mergeTeams = (newTeams: Team[] ): Team[] => {
-    const filteredTeams = teams.filter((team: Team) => !newTeams.some(newTeam => newTeam.indent === team.indent));
-
-    return [...newTeams, ...filteredTeams];
-  };  
+  const [search, setSearch] = useState<string>('');
 
   return (
     <PanelTemplate style={[s.wrapper]}>
-      <div className={s.bar}>
-        <Input
-          image={loading
-            ? 'https://cdn.impactium.fun/ui/action/redo.svg'
-            : 'https://cdn.impactium.fun/ui/specific/mention.svg'
-          }
-          placeholder={lang.team.enter_indent_or_title}
-          aria-label="Search"
-          aria-invalid="false"
-          type="search"
-          value={searchValue}
-          style={[loading && s.loading]}
-          onChange={handleSearchChange}
-        />
-        <GeistButton
-          options={{
-            type: GeistButtonTypes.Button,
-            do: () => spawnBanner(<CreateTeam />),
-            text: lang.create.team,
-            focused: true,
-            style: [s.button]
-          }}
-        />
-      </div>
-      <Panel heading={lang.team.yours}>
-        {user.teams?.map((team: Team) => {
-          <TeamUnit key={team.indent} team={team} />
-        })}
-      </Panel>
+      <SearchBar search={search} setSearch={setSearch} setTeams={setTeams} teams={teams} />
+      {/* Команды пользователя */}
+      {user.teams ?
+        <Panel heading={lang.team.yours}>
+          {user.teams?.map((team: Team) => {
+            <TeamUnit key={team.indent} team={team} />
+          })}
+        </Panel>
+      : null}
+      {/* Рекомендации */}
       <Panel heading={lang.team.recomendations} styles={[s.recomendations]}>
-        {searchValue.length > 0 ? (teams.filter((team: Team) => team.indent.toLowerCase().includes(searchValue.toLowerCase()) || team.title.toLowerCase().includes(searchValue.toLowerCase())).map((team: Team) =>
+        {search.length > 0
+          ? (teams.filter((team: Team) => {
+            return team.indent.toLowerCase().includes(search.toLowerCase()) || team.title.toLowerCase().includes(search.toLowerCase())
+          }).map((team: Team) =>
           <TeamUnit key={team.indent} team={team} />
-        )) : (teams.map((team) => (
+        )) : (teams.map((team: Team) => (
           <TeamUnit key={team.indent} team={team} />
         )))}
       </Panel>
