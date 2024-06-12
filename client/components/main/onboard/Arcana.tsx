f9d5cdeec6d73420ee2803159082306e6137c3a8
Mireg-V <markgerasimchuk8@gmail.com>
2024-06-12 16:06:53 +0200

    .

--- client/components/main/onboard/Arcana.tsx
+++
@@ -1,9 +0,0 @@
-import s from './Onboard.module.css'
-
-export function Arcana() {
-  return (
-    <div className={s.arcana}>
-      <img src='https://i.ytimg.com/vi/TaWvTM2bp_A/hqdefault.jpg'/>
-    </div>
-  )
-}
